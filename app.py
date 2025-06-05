c6b5164f7cda8f8b88c76eb00f4506d139e4c769
Trowy12 <ethan@kcd.net.au>
2025-06-05 19:53:45 +0800

    Force commit: bind app to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -56,5 +56,11 @@ def submit_quote():
 # Run Server
 # ----------------------------
 
+# Updated to bind to 0.0.0.0 for Render deployment
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
+
+
